339a5ba32155818c21386f168bd4db9e5adcb79c
paolaPerez00 <ginapaolaperez.04@gmail.com>
2021-12-15 22:17:09 -0500

    Se instaló cors

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+
 require('../drivers/mongo-connect')
 
 // Settings
 app.set('port', process.env.PORT || 4000)
 
 //Middleware
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
